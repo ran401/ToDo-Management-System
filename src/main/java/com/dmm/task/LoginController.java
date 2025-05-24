136d2ddc2d7fc42fe384043ac7c7ba06dda31b3f
KanokoTsuchimura <205731749+ran401@users.noreply.github.com>
2025-05-24 15:54:18 +0900

    課題提出

---
+++ src/main/java/com/dmm/task/LoginController.java
@@ -0,0 +1,14 @@
+package com.dmm.task;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/loginForm")
+	String loginForm() {
+		return "login";
+	}
+	
+}
